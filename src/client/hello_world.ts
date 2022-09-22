f14f8af2017d38549d238f9019af54175eaa451d
Gurianos <prevost.gregory@gmail.com>
2022-09-23 00:06:48 +0200

    first puch

--- src/client/hello_world.ts
+++ src/client/hello_world.ts
@@ -92,6 +92,12 @@ export async function establishConnection(): Promise<void> {
    version from the connection object. */
 
   //Insert the Step 1 code from the tutorial here
+  // Step 1: Connect to the Solana Devnet
+connection = new Connection(clusterApiUrl("devnet"),"confirmed");
+const version = await connection.getVersion();
+console.log('Connection to cluster established:', version);
+
+
 }
 
 /**
@@ -102,11 +108,21 @@ export async function establishPayer(): Promise<void> {
   //Step 2: Generate a keypair - this would be an account that pays for the calls to the program
   
   //Insert the Step 2 code from the tutorial here
-  
+  //Step 2: Generate a keypair - this would be an account that pays for the calls to the program
+payer = Keypair.generate();
+console.log("Public Key of Payer is:", payer.publicKey);
+
   //Step 3: Requesting an airdrop
 
    //Insert the Step 3 code from the tutorial here
-   
+   //Step 3: Requesting an airdrop
+const sig = await connection.requestAirdrop(
+  payer.publicKey,
+  2 * LAMPORTS_PER_SOL,
+);
+
+await connection.confirmTransaction(sig);
+
   console.log(
     'Using account',
     payer.publicKey.toBase58(),
@@ -203,10 +219,22 @@ export async function sayHello(): Promise<void> {
   // STEP 4: Create an instruction to be sent to the program
   
   //Insert the Step 4 code from the tutorial here
+// STEP 4: Create an instruction to be sent to the program
+const instruction = new TransactionInstruction({
+  keys: [{pubkey: greetedPubkey, isSigner: false, isWritable: true}],
+  programId,
+  data: Buffer.alloc(0), // All instructions are hellos
+});
 
   //STEP 5: Create a transaction to be sent to the blockchain containing the instruction
   
   //Insert the Step 5 code from the tutorial here
+//STEP 5: Create a transaction to be sent to the blockchain containing the instruction
+await sendAndConfirmTransaction(
+  connection,
+  new Transaction().add(instruction),
+  [payer],
+);
 
 }
 
